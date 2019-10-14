5bb20b5edfabed57bfe3ab7f7a9c84c0b948f47e
Juan Henao <parjuag@gmail.com>
2019-10-14 09:33:28 -0500

    Se cambia estructura de archivo a forma de Clase [Graph()]

--- generateGraph.py
+++ generateGraph.py
@@ -9,67 +9,71 @@ import json
 import Data
 
 
-def getStraight(axis, value, start, end):
-    """
-    Return two lists required to make a straight.
-    Params: 1) axis where the value will be.
-            2) value in the axis.
-            3) point to start on the opposite axis.
-            4) point to end on the opposite axis.
-    """
+class Graph:
+    def __init__(self, tree):
+        self.tree = tree
+        self.points = tree.getListPoints()
+        self.nodes = tree.preOrd()
 
-    axisX, axisY = [], []
-    axis = axis.lower()
-    
-    if(axis == "x"):
-        axisX = [value, value]
-        axisY = [start, end]
-    else:
-        axisX = [start, end]
-        axisY = [value, value]
-    
-    return axisX, axisY
+        # Plot Canvas Configurations
+        plt.axis([-0.5, 22.5, -0.5, 22.5])  # plt.axis([xmin, xmax, ymin, ymax])
+        plt.legend(loc="upper left")
 
+    def showPlot(self):
+        self.setPoints()
+        self.setStraights()
+        plt.show()
 
-def setPoints(points):
-    """
-    Set the points in the main plot.
-    """
+    def getStraight(self, axis, value, start, end):
+        """
+        Return two lists required to make a straight.
+        Params: 1) axis where the value will be.
+                2) value in the axis.
+                3) point to start on the opposite axis.
+                4) point to end on the opposite axis.
+        """
 
-    for point in points:
-        plt.plot(point["x"], point["y"], "bo")
-
-
-def setStraights(tree):
-    """
-    Set the straights on the main plot.
-    """
-
-    nodes = tree.preOrd()
-    straightsList = []
-
-    for node in nodes:
-        if(tree.getNodeLevel(node) % 2 == 0):
-            straightsList.append(getStraight("x", node.getValueX(), 0, 21))
+        axisX, axisY = [], []
+        axis = axis.lower()
+        
+        if(axis == "x"):
+            axisX = [value, value]
+            axisY = [start, end]
         else:
-            straightsList.append(getStraight("y", node.getValueY(), 0, 21))
-    
-    i = 0
-    for straight in straightsList:
-        plt.plot(straight[0], straight[1])
-        # Lines to save the png's of each step in the process of making the straights
-        # plt.savefig("img/"+str(nodes[i].getLabel())+".png")
-        # i += 1
+            axisX = [start, end]
+            axisY = [value, value]
+        
+        return axisX, axisY
+
+    def setPoints(self):
+        """
+        Set the points in the main plot.
+        """
+
+        for point in self.points:
+            plt.plot(point["x"], point["y"], "bo")
+
+    def setStraights(self):
+        """
+        Set the straights on the main plot.
+        """
+
+        straightsList = []
+
+        for node in self.nodes:
+            if(tree.getNodeLevel(node) % 2 == 0):
+                straightsList.append(self.getStraight("x", node.getValueX(), 0, 21))
+            else:
+                straightsList.append(self.getStraight("y", node.getValueY(), 0, 21))
+        
+        i = 0
+        for straight in straightsList:
+            plt.plot(straight[0], straight[1])
+            # Lines to save the png's of each step in the process of making the straights
+            # plt.savefig("img/"+str(nodes[i].getLabel())+".png")
+            # i += 1
 
 
 tree = Data.Data()
-points = tree.getListPoints()
-
-setPoints(points)
-setStraights(tree)
-
-# Plot Canvas Configurations
-plt.axis([-0.5, 22.5, -0.5, 22.5])  # plt.axis([xmin, xmax, ymin, ymax])
-plt.legend(loc="upper left")
-
-plt.show()
+graph = Graph(tree)
+graph.showPlot()
