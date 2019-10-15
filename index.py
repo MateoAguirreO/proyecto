b17bdfc7f4956127147bb7d236e5e1eeffee8652
Juan Henao <parjuag@gmail.com>
2019-10-14 19:06:22 -0500

    Se muestran el paso a paso de la divison

--- index.py
+++ index.py
@@ -1,24 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Parjua
+"""
+
+
 import tkinter as tk
+from PIL import Image, ImageTk
+import generateGraph
+import Data
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.master.geometry("850x600")
         self.pack()
-        self.create_widgets()
+        self.createWidgets()
+        self.canvas = None
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.tree = Data.Data()
+        self.graph = generateGraph.Graph(self.tree)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
+    def createWidgets(self):
+        self.i = 0
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.btnNextGraph = tk.Button(self)
+        self.btnNextGraph["text"] = "> Next"
+        self.btnNextGraph["command"] = self.nextGraph
+        self.btnNextGraph.pack(side="top")
+
+        self.btnResetGraph = tk.Button(self)
+        self.btnResetGraph["text"] = "Delete Graph"
+        self.btnResetGraph["command"] = self.deleteGraph
+        self.btnResetGraph.pack(side="top")
+
+        self.btnQuit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.btnQuit.pack(side="bottom")
+
+    def createCanvas(self):
+        self.canvas = tk.Canvas(self.master, width=850, height=400)
+        self.canvas.pack()
+
+    def nextGraph(self):
+        if(self.canvas):
+            if(self.i == len(self.tree.preOrd())):
+                self.i = 0
+            img1 = Image.open("img/"+str(self.i)+".png")
+            self.image1 = ImageTk.PhotoImage(img1.resize((512, 384)))
+            self.canvas.create_image(420, 200, image=self.image1)
+            self.i += 1
+        else:
+            self.createCanvas()
+            self.nextGraph()
+
+    def deleteGraph(self):
+        if(self.canvas):
+            self.canvas.destroy()
+
 
 root = tk.Tk()
 app = Application(master=root)
