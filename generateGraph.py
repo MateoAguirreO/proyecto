fa92bcb8b5844854fd7f0c79e6c556759b201875
Juan Henao <parjuag@gmail.com>
2019-10-20 15:03:58 -0500

    Creación de función 'getRectangles()'

--- generateGraph.py
+++ generateGraph.py
@@ -5,6 +5,7 @@
 
 
 import matplotlib.pyplot as plt
+import Data
 
 
 class Graph:
@@ -15,6 +16,7 @@ class Graph:
         self.tree = tree
         self.nodes = tree.preOrd()
         self.maxValue = 25
+        self.intersections = []
 
         # Plot Canvas Configurations
         plt.axis([-0.5, self.maxValue+0.5, -0.5, self.maxValue+0.5])  # plt.axis([xmin, xmax, ymin, ymax])
@@ -36,14 +38,14 @@ class Graph:
 
         axisX, axisY = [], []
         axis = axis.lower()
-        
+
         if(axis == "x"):
             axisX = [value, value]
             axisY = [start, end]
         else:
             axisX = [start, end]
             axisY = [value, value]
-        
+
         return axisX, axisY
 
     def setPoints(self):
@@ -54,20 +56,40 @@ class Graph:
         for node in self.nodes:
             plt.plot(node.getValueX(), node.getValueY(), "bo")
 
+    def getIntersections(self):
+        return self.intersections
+
     def getListStraights(self):
         """
         Returns a list of the straights
         """
 
         straightsList = []
+        self.intersections = []
+
+        straightsList.append(self.getStraight("x", 0, 0, 25))
+        straightsList.append(self.getStraight("x", 25, 0, 25))
+        straightsList.append(self.getStraight("y", 0, 0, 25))
+        straightsList.append(self.getStraight("y", 25, 0, 25))
+
+        self.intersections.append([0, 0])
+        self.intersections.append([0, 25])
+        self.intersections.append([25, 0])
+        self.intersections.append([25, 25])
 
         for node in self.nodes:
             if(self.tree.getNodeLevel(node) % 2 == 0):
                 limSouth, limNorth = self.limitVertical(straightsList, node.getValueX(), node.getValueY())
-                straightsList.append(self.getStraight("x", node.getValueX(), limSouth, limNorth))
+                line = self.getStraight("x", node.getValueX(), limSouth, limNorth)
+                self.intersections.append([node.getValueX(), limSouth])
+                self.intersections.append([node.getValueX(), limNorth])
+                straightsList.append(line)
             else:
-                limSouth, limNorth = self.limitHorizontal(straightsList, node.getValueX(), node.getValueY())
-                straightsList.append(self.getStraight("y", node.getValueY(), limSouth, limNorth))
+                limWest, limEast = self.limitHorizontal(straightsList, node.getValueX(), node.getValueY())
+                line = self.getStraight("y", node.getValueY(), limWest, limEast)
+                self.intersections.append([limWest, node.getValueY()])
+                self.intersections.append([limEast, node.getValueY()])
+                straightsList.append(line)
 
         return straightsList
 
@@ -78,7 +100,7 @@ class Graph:
         """
 
         straightsList = self.getListStraights()
-        
+
         i = 0
         for straight in straightsList:
             plt.plot(straight[0], straight[1])
@@ -109,7 +131,7 @@ class Graph:
         """
         Return one limit in the south and one in the north between a list of starights
         """
-        
+
         limSouth = 0
         limNorth = self.maxValue
         linesHorizontal = []
@@ -140,13 +162,13 @@ class Graph:
         return limSouth, limNorth
 
     def limitHorizontal(self, straightsList, valueX, valueY):
-        limSouth = 0
-        limNorth = self.maxValue
+        limWest = 0
+        limEast = self.maxValue
         linesVertical = []
         linesVerticalValueX = []
 
         if(not straightsList):
-            return limSouth, limNorth
+            return limWest, limEast
 
         # This bucle get all the horizontal lines in the plot and get its Y value
         for line in straightsList:
@@ -161,12 +183,30 @@ class Graph:
             if(lineStartY <= valueY and valueY <= lineEndY):
                 linesVerticalValueX.append(lineX)
 
-        limSouth = self.getProximValue("<", valueX, linesVerticalValueX)
-        limNorth = self.getProximValue(">", valueX, linesVerticalValueX)
+        limWest = self.getProximValue("<", valueX, linesVerticalValueX)
+        limEast = self.getProximValue(">", valueX, linesVerticalValueX)
 
-        return limSouth, limNorth
+        return limWest, limEast
 
+    def getRectangles(self):
+        lines = self.getListStraights()
+        intersections = self.getIntersections()
+        rectangles = []
+
+        for point in intersections:
+            pointX, pointY = point
+
+            if(pointX < self.maxValue and pointY > 0):
+                nothing, nextPointX = self.limitHorizontal(lines, pointX, pointY)
+                hLine, nothing2 = self.limitVertical(lines, pointX+0.01, pointY)
+
+                if([nextPointX, hLine] in intersections):
+                    rectangles.append([point, [nextPointX, hLine]])
+
+        return rectangles
 
 # tree = Data.Data()
 # graph = Graph(tree)
 # graph.setGraph()
+#
+# print(graph.getListStraights())
